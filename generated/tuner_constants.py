9e0057d4a9f236c633742458760cc3c8a20adf0b
Seb <sn@sndsfrankfort.com>
2026-03-25 16:33:54 -0400

    cancoder offsets

--- generated/tuner_constants.py
+++ generated/tuner_constants.py
@@ -149,7 +149,7 @@ class TunerConstants:
     _front_left_drive_motor_id = 7
     _front_left_steer_motor_id = 8
     _front_left_encoder_id = 14
-    _front_left_encoder_offset: units.rotation = 0.291015625
+    _front_left_encoder_offset: units.rotation = 0.292065625
     _front_left_steer_motor_inverted = True
     _front_left_encoder_inverted = False
 
@@ -160,7 +160,7 @@ class TunerConstants:
     _front_right_drive_motor_id = 1
     _front_right_steer_motor_id = 2
     _front_right_encoder_id = 11
-    _front_right_encoder_offset: units.rotation = -0.28759765625
+    _front_right_encoder_offset: units.rotation = -0.28442365625
     _front_right_steer_motor_inverted = True
     _front_right_encoder_inverted = False
 
@@ -171,7 +171,7 @@ class TunerConstants:
     _back_left_drive_motor_id = 5
     _back_left_steer_motor_id = 6
     _back_left_encoder_id = 13
-    _back_left_encoder_offset: units.rotation = 0.37841796875
+    _back_left_encoder_offset: units.rotation = 0.38244296875
     _back_left_steer_motor_inverted = True
     _back_left_encoder_inverted = False
 
